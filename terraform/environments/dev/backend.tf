7bda11530491d20ea5268d6cc88a8be730e4be02
leeym27 <yongmin2739@gmail.com>
2026-08-07 15:31:10 +0900

    feat: M1-2 S3 및 DynamoDB 테라폼 백엔드 구축 완료

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -7,3 +7,13 @@
 #     dynamodb_table = "team01-terraform-lock"
 #   }
 # }
+
+terraform {
+  backend "s3" {
+    bucket         = "likelion-team01-tfstate-ap-northeast-3" # S3 버킷명
+    key            = "dev/terraform.tfstate"                  # S3 내부 저장 경로
+    region         = "ap-northeast-3"                         # 오사카 리전
+    encrypt        = true
+    dynamodb_table = "likelion-team01-tfstate-lock"           # DynamoDB 테이블명
+  }
+}
