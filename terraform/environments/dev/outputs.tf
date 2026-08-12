4f525f38015ccd697c6e295ec28c3fe41d4e0b6b
leeym27 <yongmin2739@gmail.com>
2026-08-12 10:44:28 +0900

    중복 수정

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -80,11 +80,6 @@ output "redis_port" {
   value       = module.elasticache.redis_port
 }
 
-output "github_actions_role_arn" {
-  description = "Backend GitHub Actions가 assume할 IAM Role ARN"
-  value       = module.github_oidc.github_actions_role_arn
-}
-
 output "eks_cluster_name" {
   description = "EKS 클러스터 이름"
   value       = module.eks.cluster_name
